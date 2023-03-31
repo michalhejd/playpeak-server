fd35e0c6ab4c6652802fa9f128bab5424199099c
Michal Hejduk <michal@Michal-MacBook-Pro.local>
2023-03-31 16:25:28 +0200

    connection to db is working, removed console log

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import cors from "cors";
 import morgan from "morgan";
-//import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import v1 from "./versions/v1/router.js";
 
-/*mongoose.set("strictQuery", false);
-mongoose.connect(process.env.DB || "mongodb://localhost:27017/ssps", () =>{
-    console.log("Connected to DB");
+
+mongoose.connect(process.env.DB || "mongodb://localhost:27017/ssps", {
+}).then(() => {
+    console.log("Connected to db");
+}).catch((err) => {
+    console.log(err);
 });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-*/
+
 const app = express();
 app.use(cors());
 app.use(express.json());
--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,6 @@ config()
 
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(process.env.NODE_ENV)
     if (process.env.NODE_ENV == 'development')
         console.log(`Server running on port ${process.env.PORT || 3000}`);
     else
