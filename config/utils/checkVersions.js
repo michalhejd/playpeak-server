bd2b1d7ffb3e7c6b758ae4a4835d7302422ce9c9
michalhejd <71251670+michalhejd@users.noreply.github.com>
2023-04-10 16:06:55 +0200

    added node checker
    - added node checker

--- config/utils/checkVersions.js
+++ config/utils/checkVersions.js
@@ -2,6 +2,10 @@ import { config } from "dotenv"
 config()
 
 export async function checkVerisons(){
+    if(!process.versions.node){
+        console.log("You need to install nodejs first")
+        return process.exit(0)
+    }
     if(parseInt(process.versions.node.split(".")[0]) < 16){
         console.log("Node version is too old, please update to 16.0.0 or higher")
         return process.exit(0)
