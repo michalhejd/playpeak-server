aba35cb167c1ac81fbdf5f28e59091f7f87cfd6b
Lukas <91434573+Luksanss@users.noreply.github.com>
2023-04-19 20:15:46 +0200

    new .env checker

--- config/utils/checkEnvironments.js
+++ config/utils/checkEnvironments.js
@@ -1,45 +1,48 @@
 export async function checkEnvironments(){
-    let missingEnvironments = []
+    let missingEnvironments = [];
 
     if(!process.env.PORT){
-        missingEnvironments.push("PORT")
+        missingEnvironments.push("PORT");
     }
     if(!process.env.NODE_ENV){
-        missingEnvironments.push("NODE_ENV")
+        missingEnvironments.push("NODE_ENV");
     }
     if(!process.env.DB){
-        missingEnvironments.push("DB")
+        missingEnvironments.push("DB");
     }
     if(!process.env.JWT){
-        missingEnvironments.push("JWT")
+        missingEnvironments.push("JWT");
     }
     if(!process.env.SALT_ROUNDS){
-        missingEnvironments.push("SALT_ROUNDS")
+        missingEnvironments.push("SALT_ROUNDS");
     }
     if(!process.env.ADMIN_EMAIL){
-        missingEnvironments.push("ADMIN_EMAIL")
+        missingEnvironments.push("ADMIN_EMAIL");
     }
     if(!process.env.EMAIL_HOST){
-        missingEnvironments.push("EMAIL_HOST")
+        missingEnvironments.push("EMAIL_HOST");
     }
     if(!process.env.EMAIL_PORT){
-        missingEnvironments.push("EMAIL_PORT")
+        missingEnvironments.push("EMAIL_PORT");
     }
     if(!process.env.EMAIL_SECURE){
-        missingEnvironments.push("EMAIL_SECURE")
+        missingEnvironments.push("EMAIL_SECURE");
     }
     if(!process.env.EMAIL_USER){
-        missingEnvironments.push("EMAIL_USER")
+        missingEnvironments.push("EMAIL_USER");
     }
     if(!process.env.EMAIL_PASS){
-        missingEnvironments.push("EMAIL_PASS")
+        missingEnvironments.push("EMAIL_PASS");
     }
     if(!process.env.EMAIL_VERIFICATION_URL){
-        missingEnvironments.push("EMAIL_VERIFICATION_URL")
+        missingEnvironments.push("EMAIL_VERIFICATION_URL");
+    }
+    if(!process.env.ORIGIN){
+        missingEnvironments.push("ORIGIN");
     }
     if(missingEnvironments.length > 0){
-        console.log("Missing environments: " + missingEnvironments.join(", "))
-        return process.exit(0)
+        console.log("Missing environments: " + missingEnvironments.join(", "));
+        return process.exit(0);
     }
-    return
+    return;
 }
