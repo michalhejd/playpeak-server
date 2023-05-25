cadc7cfaef8b207a5bbd263bfbc54458bda0db5c
Lukas <91434573+Luksanss@users.noreply.github.com>
2023-05-26 00:52:20 +0200

    bugs, typos
    Co-Authored-By: michalhejd <71251670+michalhejd@users.noreply.github.com>

--- versions/v1/src/Teams/controllers/teams.js
+++ versions/v1/src/Teams/controllers/teams.js
@@ -29,7 +29,7 @@ router.get("/@me", async (req, res) => {
 });
 
 // get all requests from user to teams (inbox for requests) - only reciever can get
-router.get("/@me/requests/recived", async (req, res) => {
+router.get("/@me/requests/sent", async (req, res) => {
     if (!req.user) throw new Error(responseErrors.unauthorized);
     const user = await checkUser(req.user);
     const requests = await Invitation.find({ fromUser: user.id, type: invType.request });
@@ -37,7 +37,7 @@ router.get("/@me/requests/recived", async (req, res) => {
 });
 
 // get all invitations for user (inbox for invitations) - only receiver can get
-router.get("/@me/invitations/recived", async (req, res) => {
+router.get("/@me/invitations/received", async (req, res) => {
     if (!req.user) throw new Error(responseErrors.unauthorized);
     const user = await checkUser(req.user);
     const invitations = await Invitation.find({ toUser: user.id, type: invType.invitation });
@@ -52,7 +52,7 @@ router.get("/@me/invitations/sent", async (req, res) => {
 });
 
 // get all requests from team to user (inbox for requests) - only reciever can get
-router.get("/@me/requests/sent", async (req, res) => {
+router.get("/@me/requests/received", async (req, res) => {
     if (!req.user) throw new Error(responseErrors.unauthorized);
     const user = await checkUser(req.user);
     const requests = await Invitation.find({ toUser: user.id, type: invType.request });
